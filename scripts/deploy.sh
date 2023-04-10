21a4edbe69a2aaeaa21d9c630321885ed34cbf1f
dolotinaelvira1 <Vovchuk123>
2023-04-10 16:27:27 +0200

    flow changed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,64 +9,83 @@ export TARGET_BRANCH="master"
 # Get the commit hash for the latest commit
 COMMIT_HASH=$(git rev-parse HEAD)
 
-FLOW_FILES=$(git diff-tree --no-commit-id --name-only -r $COMMIT_HASH | grep -E '^[^.]+\.(flow-meta\.xml)$' | xargs basename)
-
-if [ -z "$FLOW_FILES" ]; then
-  echo "No changes found in commit."
-else
-  echo "flow files: $FLOW_FILES"
-
-
-
 USERNAME=$(git config --get remote.origin.url | awk -F'/' '{print $4}')
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 HEAD="$USERNAME:$BRANCH"
 
+FLOW_CHANGES=$(python -c "import os, subprocess, sys
+                          from xml.etree import ElementTree as ET
+                          import difflib
+                          import tempfile
 
-# Generate a random string for the scratch org alias
-RANDOM_STRING=$(openssl rand -hex 5)
-echo "Scratch org alias: $RANDOM_STRING"
+                          if len(sys.argv) < 2:
+                              print('Error: Commit hash not provided.')
+                              sys.exit(1)
 
-# Authenticate with Salesforce using JWT flow
-sfdx force:auth:jwt:grant --client-id=3MVG9t0sl2P.pBypyUQ9QtrDHltVGOGkJTU5Zjv_F8c22JCzQS2P8ZVqlmUgcbkTqh5UyJt..B2Er9OUeDZGZ --jwt-key-file=C:/Users/dolot/JWT/server.key --username=dolotinaelvira@empathetic-badger-rllf1u.com --set-default-dev-hub  --alias=DevHub
+                          commit_hash = sys.argv[1]
 
+                          def get_commit_changes(commit_hash):
+                              changes_list = []
+                              try:
+                                  output = subprocess.check_output(['git', 'diff', commit_hash + '^', commit_hash]).decode('utf-8')
+                                  lines = output.splitlines()
+                                  for line in lines:
+                                      if line.startswith('---') or line.startswith('+++'):
+                                          status = 'edited' if line.startswith('---') else 'added'
+                                          if line.endswith('/dev/null'):
+                                              status = 'deleted'
+                                          change = line.split(' ')[1][2:]
+                                          if not change in changes_list:
+                                              changes_list.append((status, change))
+                              except subprocess.CalledProcessError as e:
+                                  print('Error: {}'.format(e.output.decode("utf-8")))
+                                  sys.exit(1)
+                              return changes_list
 
+                          def get_flow_diff(file1, file2):
+                              root1 = ET.parse(file1).getroot()
+                              root2 = ET.parse(file2).getroot()
 
-# Create a new scratch org
-sfdx force:org:create -f "$SCRATCH_ORG_DEFINITION" --setalias $RANDOM_STRING --durationdays 7 -a $RANDOM_STRING
+                              xml1 = ET.tostring(root1, encoding='unicode', method='xml')
+                              xml2 = ET.tostring(root2, encoding='unicode', method='xml')
 
+                              d = difflib.Differ()
+                              diff = list(d.compare(xml1.splitlines(), xml2.splitlines()))
 
-sfdx force:source:push -u $RANDOM_STRING
+                              formatted_diff = []
+                              for i, line in enumerate(diff):
+                                  if line.startswith('+ ') or line.startswith('- '):
+                                      tag = line[2:].strip()
+                                      if not tag.startswith('<'):
+                                          continue
+                                      operation = 'added' if line.startswith('+ ') else 'removed'
+                                      formatted_diff.append('{} flow element: {}'.format(operation, tag))
 
+                              return formatted_diff
 
+                          flow_changes = []
+                          for status, change in get_commit_changes(commit_hash):
+                              if change.endswith('.flow-meta.xml') and status == 'edited':
+                                  old_file = tempfile.NamedTemporaryFile(delete=False).name
+                                  new_file = tempfile.NamedTemporaryFile(delete=False).name
+                                  os.system('git show {}^:{} > {}'.format(commit_hash, change, old_file))
+                                  os.system('git show {}:{} > {}'.format(commit_hash, change, new_file))
+                                  flow_changes += get_flow_diff(old_file, new_file)
 
-# Open the flows in the scratch org and retrieve their URLs
-FLOW_URLS=""
-for FILE in $FLOW_FILES; do
-   FLOW_NAME=$(grep -oP '(?<=<label>)[^<]+' "$SOURCE_PATH/flows/$FILE")
-  FLOW_URL=$(sfdx force:org:open -p "lightning/flow/$FLOW_NAME" -u $RANDOM_STRING  --urlonly)
-  FLOW_URLS+="\n$FLOW_NAME: $FLOW_URL"
-done
+                              formatted_flow_changes = "\n".join(flow_changes)
+                              print(formatted_flow_changes)
+                              ")
 
-echo "flow name: $FLOW_NAME"
-echo "flow url : $FLOW_URL"
-SCRATCH_ORG_URL=$(sfdx force:org:open -u $RANDOM_STRING --urlonly)
-echo "SCRATCH_ORG_URL : $SCRATCH_ORG_URL"
-# Create a pull request with links to the flows in the scratch org
-COMMENT="Please review the following flows in the scratch org at $SCRATCH_ORG_URL:$FLOW_URLS
-credentials to access: "
+COMMENT="Please review the following flows in the scratch org at ... credentials to access:
+Flow changes:$FORMATTED_FLOW_CHANGES"
 
-COMMENT=$(echo -e "$COMMENT" | sed 's/$/\\<br\\>/')
-COMMENT=${COMMENT//$'\n'/}
-
-echo "Comment: $COMMENT"
-echo "Head: $HEAD"
+ESC_COMMENT=$(echo -e "$COMMENT" | sed -e 's/\\/\\\\/g' -e 's/"/\\"/g' -e 's/\//\\\//g' -e 's/$/\\n/g')
 RESPONSE=$(curl -sS -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/$GITHUB_REPOSITORY/pulls \
-  -d "{\"title\":\"Peer review for declarative changes\",\"body\":\"$COMMENT\",\"head\":\"$HEAD\",\"base\":\"$TARGET_BRANCH\"}")
+  "https://api.github.com/repos/$GITHUB_REPOSITORY/pulls" \
+  -d "{\"title\":\"Peer review for declarative changes\",\"body\":\"$ESC_COMMENT\",\"head\":\"$HEAD\",\"base\":\"$TARGET_BRANCH\"}")
+
 echo "Response: $RESPONSE"
 
-PR_URL=$(echo "$RESPONSE" | grep "\"html_url\":" | awk '{print $2}' | tr -d '",')
-echo "Pull request created: $PR_URL"
-fi
+PR_URL=$(echo "$RESPONSE" | grep ""html_url":" | awk '{print $2}' | tr -d '",')
+echo "Pull request created: $PR_URL "
